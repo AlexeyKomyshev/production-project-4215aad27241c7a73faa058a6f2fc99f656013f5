5c085ca4f0db946e71a781dd13aaeb2895bb1856
AlexeyKomyshev <aleksejkomysev36@gmail.com>
2024-09-24 17:29:18 +0300

    add lazy loading, add reducerManager, add DynamicModuleLoader

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,18 +1,29 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'enteties/Counter';
 import { userReducer } from 'enteties/User';
-import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+import { createReducerManager } from 'app/providers/StoreProvider/config/reducerManager';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateSchema) {
+export function createReduxStore(
+    initialState?: StateSchema,
+    asyncReducres?:ReducersMapObject<StateSchema>,
+) {
     const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducres,
         counter: counterReducer,
         user: userReducer,
-        loginForm: loginReducer,
     };
-    return configureStore<StateSchema>({
-        reducer: rootReducers,
+
+    const reducerManager = createReducerManager(rootReducers);
+
+    const store = configureStore<StateSchema>({
+        reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
+
+    // @ts-ignore
+    store.reducerManager = reducerManager;
+
+    return store;
 }
