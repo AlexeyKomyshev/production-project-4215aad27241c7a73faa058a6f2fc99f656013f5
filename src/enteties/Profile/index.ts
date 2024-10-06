8958394627f2d33c76e5ff8433b83eb8f333d618
AlexeyKomyshev <aleksejkomysev36@gmail.com>
2024-10-06 18:18:14 +0300

    update profile, private routes, ui select, country select, currency select

--- src/enteties/Profile/index.ts
+++ src/enteties/Profile/index.ts
@@ -1,3 +1,5 @@
+import { updateProfileData } from 'enteties/Profile/model/services/updateProfileData/updateProfileData';
+
 export {
     Profile,
     ProfileSchema,
@@ -13,5 +15,15 @@ export {
 } from './model/services/fetchProfileData/fetchProfileData';
 
 export {
+    updateProfileData,
+} from './model/services/updateProfileData/updateProfileData';
+
+export {
     ProfileCard,
 } from './ui/ProfileCard/ProfileCard';
+
+export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getProfileIsLoading';
+export { getProfileError } from './model/selectors/getProfileError/getProfileError';
+export { getProfileData } from './model/selectors/getProfileData/getProfileData';
+export { getProfileReadonly } from './model/selectors/getProfileReadonly/getProfileReadonly';
+export { getProfileForm } from './model/selectors/getProfileForm/getProfileForm';
